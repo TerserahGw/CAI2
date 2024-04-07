a0ac5b9f11fae4dba91307a25e506444dd6597d0
TerserahGw <120995106+TerserahGw@users.noreply.github.com>
2024-04-07 16:04:45 +0700

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,23 +2,44 @@ from flask import Flask, jsonify, request, render_template
 from characterai import PyCAI
 
 app = Flask(__name__, template_folder='.')
-token = None
+token_main = '29422450f9ebdf864bb798a6f9796cdab019d9f1'  # Token dari kode 1
+token_api2 = None
 
-def get_client():
-    return PyCAI(token)
-    
+def get_api2_client():
+    global token_api2
+    return PyCAI(token_api2)
+
 @app.route('/')
-def index():
-    return ''
-    
-@app.route('/newchat')
+def welcome():
+    return render_template('index.html')
+
+@app.route('/api')
+def home():
+    global token_main
+    client = PyCAI(token_main)
+    return render_template('oldhome.html')
+
+@app.route('/home')
+def api():
+    return render_template('home.html')
+
+@app.route('/api/search')
+def search_character():
+    global token_main
+    client = PyCAI(token_main)
+    query = request.args.get('q', '')
+
+    if not query:
+        return jsonify({'error': 'Query is required'}), 400
+
+    results = client.character.search(query)
+
+    return jsonify(results), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+@app.route('/api/newchat')
 def new_chat():
-    global token
-    token = request.args.get('token')
-    if not token:
-        return jsonify({'error': 'Token is required'}), 400
-    
-    client = get_client()
+    global token_main
+    client = PyCAI(token_main)
     char_id = request.args.get('q', '')
 
     if not char_id:
@@ -28,14 +49,39 @@ def new_chat():
 
     return jsonify(data), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
-@app.route('/cai')
-def cai_chat():
-    global token
-    token = request.args.get('token')
-    if not token:
-        return jsonify({'error': 'Token is required'}), 400
+@app.route('/api/trending')
+def trending_characters():
+    global token_main
+    client = PyCAI(token_main)
+    trending = client.character.trending()
+
+    return jsonify(trending), 200, {'Content-Type': 'application/json; charset=utf-8'}
     
-    client = get_client()
+@app.route('/api/rec')
+def rec_characters():
+    global token_main
+    client = PyCAI(token_main)
+    rec = client.character.recommended()
+
+    return jsonify(rec), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+@app.route('/api/info')
+def info_character():
+    global token_main
+    client = PyCAI(token_main)
+    char_id = request.args.get('id', '')
+
+    if not char_id:
+        return jsonify({'error': 'Character ID is required'}), 400
+
+    info = client.character.info(char_id)
+
+    return jsonify(info), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+@app.route('/api/cai')
+def cai_chat():
+    global token_main
+    client = PyCAI(token_main)
     char_id = request.args.get('charid', '')
     message = request.args.get('message', '')
 
@@ -59,14 +105,62 @@ def cai_chat():
 
     return jsonify({'name': name, 'reply': text}), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
-@app.route('/delcai')
-def delete_chat():
-    global token
-    token = request.args.get('token')
-    if not token:
+@app.route('/api2/newchat')
+def new_chat_api2():
+    global token_api2
+    token_api2 = request.args.get('token')
+    if not token_api2:
         return jsonify({'error': 'Token is required'}), 400
     
-    client = get_client()
+    client = get_api2_client()
+    char_id = request.args.get('charid', '')
+
+    if not char_id:
+        return jsonify({'error': 'Character ID is required'}), 400
+
+    data = client.chat.new_chat(char_id)
+
+    return jsonify(data), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+@app.route('/api2/cai')
+def cai_chat_api2():
+    global token_api2
+    token_api2 = request.args.get('token')
+    if not token_api2:
+        return jsonify({'error': 'Token is required'}), 400
+    
+    client = get_api2_client()
+    char_id = request.args.get('charid', '')
+    message = request.args.get('message', '')
+
+    if not char_id:
+        return jsonify({'error': 'Character ID is required'}), 400
+    if not message:
+        return jsonify({'error': 'Message is required'}), 400
+
+    chat = client.chat.get_chat(char_id)
+    participants = chat['participants']
+
+    if not participants[0]['is_human']:
+        tgt = participants[0]['user']['username']
+    else:
+        tgt = participants[1]['user']['username']
+
+    data = client.chat.send_message(chat['external_id'], tgt, message)
+
+    name = data['src_char']['participant']['name']
+    text = data['replies'][0]['text']
+
+    return jsonify({'name': name, 'reply': text}), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+@app.route('/api2/delcai')
+def delete_chat():
+    global token_api2
+    token_api2 = request.args.get('token')
+    if not token_api2:
+        return jsonify({'error': 'Token is required'}), 400
+    
+    client = get_api2_client()
     history_id = request.args.get('history_id', '')
     uuids_to_delete = request.args.get('uuids_to_delete', '')
 
@@ -79,14 +173,14 @@ def delete_chat():
 
     return jsonify(result), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
-@app.route('/histories')
+@app.route('/api2/histories')
 def get_histories():
-    global token
-    token = request.args.get('token')
-    if not token:
+    global token_api2
+    token_api2 = request.args.get('token')
+    if not token_api2:
         return jsonify({'error': 'Token is required'}), 400
     
-    client = get_client()
+    client = get_api2_client()
     char_id = request.args.get('charid', '')
 
     if not char_id:
@@ -96,14 +190,14 @@ def get_histories():
 
     return jsonify(histories), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
-@app.route('/history')
+@app.route('/api2/history')
 def get_history():
-    global token
-    token = request.args.get('token')
-    if not token:
+    global token_api2
+    token_api2 = request.args.get('token')
+    if not token_api2:
         return jsonify({'error': 'Token is required'}), 400
     
-    client = get_client()
+    client = get_api2_client()
     history_id = request.args.get('history_id', '')
 
     if not history_id:
@@ -114,4 +208,4 @@ def get_history():
     return jsonify(history), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
